058f02f424b8061c7aee83dc10676988b3a2bd39
Samuel Alexandrino <samuelalexandrinocontato@gmail.com>
2025-11-01 21:28:53 -0300

    Campanha v23: Corrigir falha de aninhamento de Roteador (Router in Router)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// --- INÍCIO DA CORREÇÃO v23 ---
+// REMOVIDO: import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+// --- FIM DA CORREÇÃO v23 ---
 import { AuthProvider } from './context/AuthContext';
 
 // Nossos componentes de página
@@ -8,43 +11,37 @@ import UploadPage from './components/UploadPage';
 import HistoryPage from './components/HistoryPage';
 import ProtectedRoute from './components/ProtectedRoute'; // O novo guarda
 
-// (Importe seu Sidebar se ele for separado)
-// import Sidebar from './components/Sidebar'; 
-
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <div className="app-container">
-          {/* O Sidebar agora é renderizado DENTRO das rotas protegidas,
-            para que não apareça na página de login.
-          */}
-          <Routes>
-            {/* Rota Pública */}
-            <Route path="/" element={<LoginPage />} />
+      {/* --- INÍCIO DA CORREÇÃO v23 --- */}
+      {/* O <BrowserRouter> foi removido daqui. 
+          Ele agora vive (e deve viver) no arquivo 'src/index.js' */}
+      <div className="app-container">
+        <Routes>
+          {/* Rota Pública */}
+          <Route path="/" element={<LoginPage />} />
 
-            {/* Rotas Protegidas */}
-            <Route 
-              path="/upload" 
-              element={
-                <ProtectedRoute>
-                  <UploadPage />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/history" 
-              element={
-                <ProtectedRoute>
-                  <HistoryPage />
-                </ProtectedRoute>
-              } 
-            />
-            
-            {/* TODO: Adicionar uma rota "Not Found" 404 */}
-          </Routes>
-        </div>
-      </BrowserRouter>
+          {/* Rotas Protegidas */}
+          <Route 
+            path="/upload" 
+            element={
+              <ProtectedRoute>
+                <UploadPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/history" 
+            element={
+              <ProtectedRoute>
+                <HistoryPage />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </div>
+      {/* --- FIM DA CORREÇÃO v23 --- */}
     </AuthProvider>
   );
 }
